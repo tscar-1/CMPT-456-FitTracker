82ecc9cf7e1bfeb0476962fd0b2f21ab4c02685d
tscar-1 <71184141+tscar-1@users.noreply.github.com>
2023-12-04 11:58:00 -0500

    Update Workout.java

--- src/core/Workout.java
+++ src/core/Workout.java
@@ -1,5 +1,12 @@
 package core;
 
 public class Workout {
-  
+  private String name;
+  private List<Exercise> workout;
+
+  public Workout() {
+    workout = new ArrayList<>();
+  }
+
+  public void 
 }
