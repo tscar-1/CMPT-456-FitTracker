207ed4eb8e749aec910e74a31874f9e9b265a3c9
tscar-1 <71184141+tscar-1@users.noreply.github.com>
2023-12-04 23:47:22 -0500

    Update Exercise.java

--- src/core/Exercise.java
+++ src/core/Exercise.java
@@ -4,78 +4,79 @@ import java.io.File;
 import javax.imageio.ImageIO;
 
 public class Exercise {
-  private String name;
-  private String recordType;
-  private String muscleType;
-  private String muscleGroup;
-  private Image image;
 
-  //Constructor
-  public Exercise(String name, String recordType, String muscleType, String muscleGroup, Image image) {
-    this.name = name;
-    this.recordType = recordType;
-    this.muscleType = muscleType;
-    this.muscleGroup = muscleGroup;
-    this.image = image;
-  }
+    private String name;
+    private String recordType;
+    private String muscleType;
+    private String muscleGroup;
+    //private Image image;
 
-  //Getters and setters
-  public String getName() {
-    return name;
-  }
+    //Constructor
+    public Exercise(String name, String recordType, String muscleType, String muscleGroup/*, Image image*/) {
+        this.name = name;
+        this.recordType = recordType;
+        this.muscleType = muscleType;
+        this.muscleGroup = muscleGroup;
+        //this.image = image;
+    }
 
-  public setName(String name) {
-    this.name = name;
-  }
+    //Setters and getters
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  public String getRecordType() {
-    return recordType;
-  }
+    public String getName() {
+        return name;
+    }
+    
+    public void setRecordType(String recordType) {
+        this.recordType = recordType;
+    }
 
-  public setRecordType(String recordType) {
-    this.recordType = recordType;
-  }
+    public String getRecordType() {
+        return recordType;
+    }
 
-  public String getMuscleType() {
-    return muscleType;
-  }
+    public void setMuscleType(String muscleType) {
+        this.muscleType = muscleType;
+    }
+    
+    public String getMuscleType() {
+        return muscleType;
+    }
+    
+    public void setMuscleGroup(String muscleGroup) {
+        this.muscleGroup = muscleGroup;
+    }
 
-  public setMuscleType(String muscleType) {
-    this.muscleType = muscleType;
-  }
+    public String getMuscleGroup() {
+        return muscleGroup;
+    }
 
-  public String getMuscleGroup() {
-    return muscleGroup;
-  }
+    /*public String getImage() {
+        return image;
+    }*/
 
-  public setMuscleGroup(String muscleGroup) {
-    this.muscleGroup = muscleGroup;
-  }
+    /*public setImage(Image image) {
+        this.image = ImageIO.read(new File("images/" + image + ".png"));
+    }*/
 
-  public String getImage() {
-    return image;
-  }
+    /*public void addExercise(Exercise exercise, String username) {
+        exercise.add(exercise);
+        database.saveExercise(username + "_exercise_" + this.name + ".json", this);
+    }
 
-  public setImage(Image image) {
-    this.image = ImageIO.read(new File("images/" + image + ".png"));
-  }
+    public void removeExercise(Exercise exercise, String username) {
+        workout.remove(exercise);
+    }*/
 
-  public void addExercise(Exercise exercise, String username) {
-    workout.add(exercise);
-    database.saveExercise(username + "_exercise_" + this.name + ".json", this);
-  }
-
-  public void removeExercise(Exercise exercise, String username) {
-    workout.remove(exercise);
-  }
-
-  @Override
-  public String toString() {
-        return "Exercise{" +
-                "name='" + name + '\'' +
-                ", recordType='" + recordType + '\'' +
-                ", muscleType='" + muscleType + '\'' +
-                ", muscleGroup='" + muscleGroup + '\'' +
-                '}';
+    @Override
+    public String toString() {
+        return "Exercise{"
+                + "name='" + name + '\''
+                + ", recordType='" + recordType + '\''
+                + ", muscleType='" + muscleType + '\''
+                + ", muscleGroup='" + muscleGroup + '\''
+                + '}';
     }
 }
