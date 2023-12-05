233ed0bef6e9528899b3345c2eaaaf469baf126d
tscar-1 <71184141+tscar-1@users.noreply.github.com>
2023-12-04 23:47:39 -0500

    Update Workout.java

--- src/core/Workout.java
+++ src/core/Workout.java
@@ -1,31 +1,35 @@
 package core;
 
+import java.util.*;
+
 public class Workout {
-  private String name;
-  private List<Exercise> workout;
 
-  public Workout() {
-    workout = new ArrayList<>();
-  }
+    private String name;
+    private List<Exercise> workout;
 
-  public String getName() {
-    return name;
-  }
+    public Workout() {
+        workout = new ArrayList<>();
+    }
 
-  public setName(String name) {
-    this.name = name;
-  }
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return name;
+    }
 
-  public int getLength() {
-    return workout.size();
-  }
 
-  public void addExercise(Exercise exercise, String username) {
-    workout.add(exercise);
-    database.saveWorkout(username + "_workout_" + name + ".json", this);
-  }
+    public int getLength() {
+        return workout.size();
+    }
 
-  public void removeExercise(Exercise exercise, String username) {
-    workout.remove(exercise);
-  }
+    /*public void addExercise(Exercise exercise, String username) {
+        workout.add(exercise);
+        database.saveWorkout(username + "_workout_" + name + ".json", this);
+    }
+
+    public void removeExercise(Exercise exercise, String username) {
+        workout.remove(exercise);
+    }*/
 }
