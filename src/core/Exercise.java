af259598d8604190d0c3a78455ad8ccc2a03df3e
tscar-1 <71184141+tscar-1@users.noreply.github.com>
2023-12-06 01:22:33 -0500

    Update Exercise.java

--- src/core/Exercise.java
+++ src/core/Exercise.java
@@ -1,10 +1,10 @@
 package core;
 
-import java.io.File;
-import javax.imageio.ImageIO;
+import java.time.LocalDate;
 
 public class Exercise {
 
+    private LocalDate date;
     private String name;
     private int recordType; //1 weight-based, 2 distance-based
     private int muscleType; //1 triceps, 2 chest, 3 shoulders, 4 biceps, 5 core, 6 back, 7 forearms, 8 upper legs, 9 glutes, 10 cardio, 11 lower legs
@@ -13,17 +13,18 @@ public class Exercise {
     private int reps;
     private int distance;
     private int duration;
-    
 
     //Constructor
-    public Exercise(String name, int recordType, int muscleType) {
+    public Exercise(LocalDate date, String name, int recordType, int muscleType) {
+        this.date = date;
         this.name = name;
         this.recordType = recordType;
         this.muscleType = muscleType;
     }
     
     //Weight-based Constructor
-    public Exercise(String name, int recordType, int muscleType, int weight, int sets, int reps) {
+    public Exercise(LocalDate date, String name, int recordType, int muscleType, int weight, int sets, int reps) {
+        this.date = date;
         this.name = name;
         this.recordType = recordType;
         this.muscleType = muscleType;
@@ -33,7 +34,8 @@ public class Exercise {
     }
     
     //Distance-based Constructor
-    public Exercise(String name, int recordType, int muscleType, int distance, int duration) {
+    public Exercise(LocalDate date, String name, int recordType, int muscleType, int distance, int duration) {
+        this.date = date;
         this.name = name;
         this.recordType = recordType;
         this.muscleType = muscleType;
@@ -42,6 +44,14 @@ public class Exercise {
     }
 
     //Setters and getters
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+    
+    public LocalDate getDate() {
+        return date;
+    }
+    
     public void setName(String name) {
         this.name = name;
     }
@@ -105,31 +115,4 @@ public class Exercise {
     public int getDuration() {
         return duration;
     }
-
-    /*public String getImage() {
-        return image;
-    }*/
-
-    /*public setImage(Image image) {
-        this.image = ImageIO.read(new File("images/" + image + ".png"));
-    }*/
-
-    /*public void addExercise(Exercise exercise, String username) {
-        exercise.add(exercise);
-        database.saveExercise(username + "_exercise_" + this.name + ".json", this);
-    }
-
-    public void removeExercise(Exercise exercise, String username) {
-        workout.remove(exercise);
-    }
-
-    @Override
-    public String toString() {
-        return "Exercise{"
-                + "name='" + name + '\''
-                + ", recordType='" + recordType + '\''
-                + ", muscleType='" + muscleType + '\''
-                + ", muscleGroup='" + muscleGroup + '\''
-                + '}';
-    }*/
 }
