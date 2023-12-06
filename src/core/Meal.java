fd2a9a65df11b0bd780dbe2b4591e651c7fbc6ee
LouieCeliberti <Louismceliberti@gmail.com>
2023-12-06 09:05:26 -0500

    Update Meal.java
    Update Meals

--- src/core/Meal.java
+++ src/core/Meal.java
@@ -1,5 +1,50 @@
 package core;
 
+import java.time.LocalDate;
+
 public class Meal {
-  
+
+    private LocalDate date;
+    private String mainMeal;
+    private String side;
+    private String drink;
+
+    public Meal(LocalDate date, String mainMeal, String side, String drink) {
+        this.date = date;
+        this.mainMeal = mainMeal;
+        this.side = side;
+        this.drink = drink;
+    }
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getMainMeal() {
+        return mainMeal;
+    }
+
+    public void setMainMeal(String mainMeal) {
+        this.mainMeal = mainMeal;
+    }
+
+    public String getSide() {
+        return side;
+    }
+
+    public void setSide(String side) {
+        this.side = side;
+    }
+
+    public String getDrink() {
+        return drink;
+    }
+
+    public void setDrink(String drink) {
+        this.drink = drink;
+    }
+
 }
