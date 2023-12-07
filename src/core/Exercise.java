b3d1d73f43a3660140970ab957c65726e7cc54f2
tscar-1 <71184141+tscar-1@users.noreply.github.com>
2023-12-07 03:38:08 -0500

    Update Exercise.java

--- src/core/Exercise.java
+++ src/core/Exercise.java
@@ -1,248 +1,104 @@
-package database;
+package core;
 
-import core.*;
-import java.lang.reflect.Type;
-import java.time.LocalDate;
-import utils.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import com.google.gson.*;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
-import com.google.gson.annotations.Expose;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.File;
+public class Exercise {
+    private String name;
+    private int recordType; //1 weight-based, 2 distance-based
+    private int muscleType; //1 triceps, 2 chest, 3 shoulders, 4 biceps, 5 core, 6 back, 7 forearms, 8 upper legs, 9 glutes, 10 cardio, 11 lower legs
+    private int weight;
+    private int set;
+    private int reps;
+    private double distance;
+    private double duration;
 
-public class User {
-    @Expose
-    private String username;
-    @Expose
-    private String email;
-    @Expose
-    private String fName;
-    @Expose
-    private String lName;
-    @Expose
-    private String password;
-
-    public User() {
-        username = "";
-        email = "";
-        fName = "";
-        lName = "";
-        password = "";
+    //Constructor
+    public Exercise(String name, int recordType, int muscleType) {
+        this.name = name;
+        this.recordType = recordType;
+        this.muscleType = muscleType;
+    }
+    
+    //Weight-based Constructor
+    public Exercise(String name, int recordType, int muscleType, int set, int weight, int reps) {
+        this.name = name;
+        this.recordType = recordType;
+        this.muscleType = muscleType;
+        this.set = set;
+        this.weight = weight;
+        this.reps = reps;
+    }
+    
+    //Distance-based Constructor
+    public Exercise(String name, int recordType, int muscleType, int set, double distance, double duration) {
+        this.name = name;
+        this.recordType = recordType;
+        this.muscleType = muscleType;
+        this.set = set;
+        this.distance = distance;
+        this.duration = duration;
     }
 
-    public User(String username, String email, String fName, String lName, String password) {
-        this.username = username;
-        this.email = email;
-        this.fName = fName;
-        this.lName = lName;
-        this.password = StringUtil.applySha256(password);
-
-        File usersFile = new File("users.json");
-        if (!usersFile.exists()) {
-            try ( FileWriter writer = new FileWriter(usersFile)) {
-                writer.write("[]");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        File exercisesFile = new File(username + "_exercises.json");
-        if (!exercisesFile.exists()) {
-            try ( FileWriter writer = new FileWriter(exercisesFile)) {
-                writer.write("[]");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        File workoutsFile = new File(username + "_workouts.json");
-        if (!workoutsFile.exists()) {
-            try ( FileWriter writer = new FileWriter(workoutsFile)) {
-                writer.write("[]");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        File foodsFile = new File(username + "_foods.json");
-        if (!foodsFile.exists()) {
-            try ( FileWriter writer = new FileWriter(foodsFile)) {
-                writer.write("[]");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        File mealsFile = new File(username + "_meals.json");
-        if (!mealsFile.exists()) {
-            try ( FileWriter writer = new FileWriter(mealsFile)) {
-                writer.write("[]");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        File progressFile = new File(username + "_progress.json");
-        if (!mealsFile.exists()) {
-            try ( FileWriter writer = new FileWriter(progressFile)) {
-                writer.write("[]");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().registerTypeAdapter(LocalDate.class, new LocalDateAdapter()).setPrettyPrinting().create();
-        List<User> userList;
-        try ( FileReader reader = new FileReader(usersFile)) {
-            userList = gson.fromJson(reader, new TypeToken<List<User>>() {}.getType());
-            if (userList == null) {
-                userList = new ArrayList<>();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            userList = new ArrayList<>();
-        }
-
-        User currentUser = this;
-        userList.add(currentUser);
-
-        try ( BufferedWriter writer = new BufferedWriter(new FileWriter(usersFile))) {
-            gson.toJson(userList, writer);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        System.out.printf("User " + username + " registered.\n");
+    //Setters and getters
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public String getName() {
+        return name;
+    }
+    
+    public void setRecordType(int recordType) {
+        this.recordType = recordType;
     }
 
-    public String getUsername() {
-        return username;
+    public int getRecordType() {
+        return recordType;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setMuscleType(int muscleType) {
+        this.muscleType = muscleType;
     }
-
-    public String getEmail() {
-        return email;
+    
+    public int getMuscleType() {
+        return muscleType;
     }
-
-    public void setFName(String fName) {
-        this.fName = fName;
+    
+    public void setWeight(int weight) {
+        this.weight = weight;
     }
-
-    public String getFName() {
-        return fName;
+    
+    public int getWeight() {
+        return weight;
     }
-
-    public void setLName(String lName) {
-        this.lName = lName;
+    
+    public void setSet(int set) {
+        this.set = set;
     }
-
-    public void setPassword(String password) {
-        this.password = StringUtil.applySha256(password);
+    
+    public int getSet() {
+        return set;
     }
-
-    public String getPassword() {
-        return this.password;
+    
+    public void setReps(int reps) {
+        this.reps = reps;
     }
-
-    private List<ProgressEntry> readProgress() {
-        File progressFile = new File(username + "_progress.json");
-        Gson gson = new GsonBuilder().registerTypeAdapter(LocalDate.class, new LocalDateAdapter()).create();
-        if (!progressFile.exists()) {
-            return new ArrayList<>();
-        }
-
-        try (FileReader reader = new FileReader(progressFile)) {
-            Type progressListType = new TypeToken<List<ProgressEntry>>(){}.getType();
-            return gson.fromJson(reader, progressListType);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return new ArrayList<>();
-        }
+    
+    public int getReps() {
+        return reps;
     }
-
-    private void writeProgress(List<ProgressEntry> progressEntries) {
-        File progressFile = new File(username + "_progress.json");
-        Gson gson = new GsonBuilder().registerTypeAdapter(LocalDate.class, new LocalDateAdapter()).setPrettyPrinting().create();
-
-        try (FileWriter writer = new FileWriter(progressFile)) {
-            gson.toJson(progressEntries, writer);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    
+    public void setDistance(double distance) {
+        this.distance = distance;
     }
-
-    public void saveProgressExerciseWeight(LocalDate date, String exerciseName, int exerciseRecordType, int exerciseMuscleType, int exerciseSetNum, int exerciseWeightAmt, int exerciseRepsAmt) {
-        Exercise exercise = new Exercise(exerciseName, exerciseRecordType, exerciseMuscleType, exerciseSetNum, exerciseWeightAmt, exerciseRepsAmt);
-        List<ProgressEntry> progressEntries = readProgress();
-
-        ProgressEntry entry = progressEntries.stream()
-                .filter(e -> e.date.equals(date))
-                .findFirst()
-                .orElseGet(() -> {
-                    ProgressEntry newEntry = new ProgressEntry(date);
-                    progressEntries.add(newEntry);
-                    return newEntry;
-                });
-
-        entry.addExercise(exercise);
-        writeProgress(progressEntries);
+    
+    public double getDistance() {
+        return distance;
     }
-
-    public void saveProgressExerciseDistance(LocalDate date, String exerciseName, int exerciseRecordType, int exerciseMuscleType, int exerciseSetNum, double exerciseDistanceAmt, double exerciseDurationLen) {
-        Exercise exercise = new Exercise(exerciseName, exerciseRecordType, exerciseMuscleType, exerciseSetNum, exerciseDistanceAmt, exerciseDurationLen);
-        //to be implemented
+    
+    public void setDuration(double duration) {
+        this.duration = duration;
     }
-
-    public void saveProgressFood(LocalDate date, String foodName, int foodCalorieAmt, int foodProteinAmt, int foodCarbsAmt) {
-
-    }
-
-    private class ProgressEntry {
-        private LocalDate date;
-        private List<Exercise> exercises;
-
-        public ProgressEntry(LocalDate date) {
-            this.date = date;
-            this.exercises = new ArrayList<>();
-        }
-
-        public void addExercise(Exercise exercise) {
-            exercises.add(exercise);
-        }
-
-        public void setDate(LocalDate date) {
-            this.date = date;
-        }
-
-        public LocalDate getDate() {
-            return date;
-        }
-    }
-
-    public class LocalDateAdapter implements JsonSerializer<LocalDate>, JsonDeserializer<LocalDate> {
-
-        @Override
-        public JsonElement serialize(LocalDate src, Type typeOfSrc, JsonSerializationContext context) {
-            return new JsonPrimitive(src.toString()); // Convert LocalDate to JSON
-        }
-
-        @Override
-        public LocalDate deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) {
-            return LocalDate.parse(json.getAsString()); // Convert JSON to LocalDate
-        }
+    
+    public double getDuration() {
+        return duration;
     }
 }
